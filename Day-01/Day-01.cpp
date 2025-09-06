1c097058715339e21757073d841ab5f2e05afe6c
Subhash <wanabecoderguy@gmail.com>
2025-09-06 20:40:21 +0530

    Day-1: Started DSA in C++

---
+++ Day-01/Day-01.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//      //isne enter daba diya. ( haha ) 
+//     return 0; //to end the code we use ; 
+// }
+
+//cout<<"Hello World"<<endl<<"Hello CW";
+
+//================================================
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     // cout<<"nn\n\nnn\n";
+//     // cout<<"nn/n/nnn/n";
+//     cout<<"4+3"<<1+6<<"Hello world"<<endl;
+// }
+
+
+// Variables and thier declaration 
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x=18;
+// }
+
---
+++ Day-01/operator.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x = 5;
+//     int y = 2;
+//     cout<<x/y;    
+// }
+
+// Increment-Decrement Operators and Comments
+#include <iostream>
+using namespace std;
+int main(){
+    int x = 5;
+    cout << ++x << endl; //pre Increment
+    cout<<x++<<endl; //post increment
+    cout<<x<<endl; //effect of post increment
+}
